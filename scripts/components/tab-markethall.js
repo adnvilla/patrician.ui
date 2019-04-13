33c47c53da36b03e0d53c9769f7cd4b828817390
AdnVilla <adnvilla@gmail.com>
2019-04-13 17:54:51 -0500

    Added save to commodities values Added update to changed city selected

--- scripts/components/tab-markethall.js
+++ scripts/components/tab-markethall.js
@@ -1,20 +1,13 @@
-Vue.component('tab-markethall',{
-    template:'#tabmarkethall',
+Vue.component('tab-markethall', {
+    template: '#tabmarkethall',
     data() {
         return {
-            commodities:[],
-            cities:[],
-            cityselected:'Aalborg'
+            commodities: [],
+            cities: [],
+            cityselected: ''
         }
     },
     created: function () {
-        this.$http.get('http://localhost:8080/commodities')
-            .then(response => {
-                response.json().then(data => {
-                    this.commodities = data
-                })
-            }, response => {
-            })
         this.$http.get('http://localhost:8080/cities')
             .then(response => {
                 response.json().then(data => {
@@ -23,9 +16,35 @@ Vue.component('tab-markethall',{
             }, response => {
             })
     },
-    methods:{
-        save: function(event){
-            console.log(this.commodities)
+    methods: {
+        save: function (event) {
+            r = Object.keys(this.commodities).map((key) => {
+                return {
+                    name: this.commodities[key].Name,
+                    buy: this.commodities[key].Buy,
+                    sell: this.commodities[key].Sell,
+                    production: this.commodities[key].Production,
+                    consumption: this.commodities[key].Consumption,
+                }
+            })
+            this.$http.post('http://localhost:8080/city/' + this.cityselected + '/commodities', {
+                commodities: r
+            }).then(response => {
+            }, response => {
+            })
+        },
+        onChangeCitySelected: function (event) {
+            if (this.cityselected === '') {
+                return
+            }
+
+            this.$http.get('http://localhost:8080/city/' + this.cityselected + '/commodities')
+                .then(response => {
+                    response.json().then(data => {
+                        this.commodities = data
+                    })
+                }, response => {
+                })
         }
     }
 })
