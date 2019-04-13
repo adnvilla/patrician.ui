c66696d05fdf68ef53882c677747419eafbd9b08
AdnVilla <adnvilla@gmail.com>
2019-04-13 17:55:26 -0500

    Added view to distances

--- scripts/components/tab-distances.js
+++ scripts/components/tab-distances.js
@@ -1,4 +1,38 @@
 
 Vue.component('tab-distances', {
-    template: '<div>Distances component</div>'
+    template: '#tabdistances',
+    data() {
+        return {
+            cities:[],
+            distances: [],
+            citiesdistances: [],
+            cityselected: ''
+        }
+    },
+    created: function () {
+        this.$http.get('http://localhost:8080/distances')
+            .then(response => {
+                response.json().then(data => {
+                    this.citiesdistances = data
+                })
+            }, response => {
+            })
+        this.$http.get('http://localhost:8080/cities')
+            .then(response => {
+                response.json().then(data => {
+                    this.cities = data
+                })
+            }, response => {
+            })
+    },
+    methods: {
+        onChangeCitySelected: function (event) {
+            this.distances = Object.keys(this.citiesdistances).filter((key) => {
+                return key === this.cityselected
+            }).reduce((obj, key) => {
+                obj[key] = this.citiesdistances[key]
+                return obj[key]
+            }, {})
+        }
+    }
 })
